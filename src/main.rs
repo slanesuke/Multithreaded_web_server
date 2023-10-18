853d5a6c2b3238034804c8f910cf709cabcbe7eb
slanesuke <slaneian@gmail.com>
2023-10-18 15:33:36 -0600

    switched the if with a match. Added a sleep duration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}};
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -12,11 +18,18 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+
+    let (status, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
+
+
+
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
