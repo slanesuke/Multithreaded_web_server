8e8a8bc0b79a00c63961a5528a607a4360e041ef
slanesuke <slaneian@gmail.com>
2023-10-19 16:48:19 -0600

    added execute fn as well as a panic if Threadpool size > 0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
+use std::thread::JoinHandle;
+
 pub struct ThreadPool;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
+        /// Create a new ThreadPool.
+        ///
+        /// The size is the number of threads in the pool.
+        ///
+        /// # Panics
+        ///
+        /// The `new` function will panic if the size is zero.
+        assert!(size > 0);
+
         ThreadPool
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+    F: FnOnce() + Send + 'static,
+    {
     }
 }
