a7620ba04d22505fc8b28496e296f4dce473fb2c
slanesuke <slaneian@gmail.com>
2023-10-24 08:50:47 -0600

    clone the Arc to bump the reference count so the workers can share ownership of the receiver

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
 use std::thread::JoinHandle;
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+fn main() {}
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
@@ -16,13 +24,17 @@ impl ThreadPool {
         /// The `new` function will panic if the size is zero.
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -42,8 +54,10 @@ pub struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Worker { id, thread }
     }
